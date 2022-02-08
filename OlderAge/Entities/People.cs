6a2d1a705ea3a9f5fb2b8548ad777e6e13385481
Jeremias Algonz <jeremiasgoncalez@gmail.com>
2022-02-07 23:55:13 -0300

    PeopleClass implemented

---
+++ OlderAge/Entities/People.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace OlderAge.Entities
+{
+    class People
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public People()
+        {
+
+        }
+
+        public People(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void ShowData(People obj)
+        {
+            People other = obj as People;
+            Console.WriteLine($"{other.Name}, {other.Age}.");
+        }
+    }
+}
---
+++ OlderAge/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using OlderAge.Entities;
+
+namespace OlderAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            People people = new People();
+
+            try 
+            {
+
+                Console.Write("Enter value to register: How many people? ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                for (int i = 1; i <= quantity; i++)
+                {
+                    Console.Write($"Enter people #{i} name: ");
+                    string name = Console.ReadLine();
+                    Console.Write($"Enter people #{i} age: ");
+                    int age = int.Parse(Console.ReadLine());
+
+                    people = new People(name, age);
+                }
+                
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"An unexpected error occurred! {e.Message}...");
+            }
+        }
+    }
+}
